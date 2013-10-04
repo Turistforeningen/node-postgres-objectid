18b09902747803d69b337ec86a2fba1dfd0c7f53
Hans Kristian Flaatten <hans@starefossen.com>
2013-10-04 17:37:44 +0200

    Completely remove need to execute as postgres

--- scripts/pg-init.sh
+++ scripts/pg-init.sh
@@ -2,8 +2,7 @@
 
 SCRIPTPATH=$(dirname $0)
 
-sudo -u postgres psql -c 'DROP DATABASE IF EXISTS test;'
-sudo -u postgres psql -c 'CREATE DATABASE test;'
-sudo -u postgres psql -c "ALTER USER postgres password '1234';"
-sudo -u postgres psql -d test -a -f $SCRIPTPATH/pg-init.sql
+psql -U postgres -w -c 'DROP DATABASE IF EXISTS test;'
+psql -U postgres -w -c 'CREATE DATABASE test;'
+psql -U postgres -w -d test -a -f $SCRIPTPATH/pg-init.sql
 
