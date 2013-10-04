84198608b848cf7f1a6f376122f4b535a997a09e
Hans Kristian Flaatten <hans@starefossen.com>
2013-10-04 16:04:54 +0200

    Add password reset for postgres user

--- scripts/pg-init.sh
+++ scripts/pg-init.sh
@@ -4,5 +4,6 @@ SCRIPTPATH=$(dirname $0)
 
 sudo -u postgres psql -c 'DROP DATABASE IF EXISTS test;'
 sudo -u postgres psql -c 'CREATE DATABASE test;'
+sudo -u postgres psql -c "ALTER USER postgres password '1234';"
 sudo -u postgres psql -d test -a -f $SCRIPTPATH/pg-init.sql
 
